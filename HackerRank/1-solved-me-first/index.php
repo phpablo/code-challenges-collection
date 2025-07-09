aa9a8127e9c3f398b308d295e46aff24ebd696ba
phpablo <pablohenrique120@gmail.com>
2025-07-09 09:15:14 -0300

    :technologist: HackerRank: Solved Challenge

---
+++ HackerRank/1-solved-me-first/index.php
@@ -0,0 +1,7 @@
+<?php
+
+// Complete the function  to compute the sum of two integers.
+function solveMeFirst($a, $b) {
+  // Hint: Type return $a + $b; below  
+  return $a + $b;
+}
---
+++ HackerRank/2-sum-array/index.php
@@ -0,0 +1,7 @@
+<?php
+// Given an array of integers, find the sum of its elements.
+function simpleArraySum($ar) {
+  // encontrar a soma do elementos
+  // iterar -> somar em um contador
+  return array_sum($ar);
+}
